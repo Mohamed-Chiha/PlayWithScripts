edc367b8ca2e7288da3dd059f2e297481fcddaa0
Mohamed-Chiha <mohamedchiha289@gmail.com>
2025-10-29 11:17:44 +0100

    OK : Initialized Project

---
+++ src/main/java/com/example/pws/PlayWithScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithScriptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayWithScriptsApplication.class, args);
+    }
+
+}
