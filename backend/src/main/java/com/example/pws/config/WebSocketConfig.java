92b4ed2725d946d33c0821c9413d95c6737bc9c5
Mohamed Chiha <84137827+Mohamed-Chiha@users.noreply.github.com>
2025-10-31 10:03:55 +0100

    Enable interactive Docker TTY for terminal websocket

--- backend/src/main/java/com/example/pws/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/pws/config/WebSocketConfig.java
@@ -1,24 +1,21 @@
 package com.example.pws.config;
 
-
-import com.example.pws.web.TerminalWebSocketHandler;
+import com.example.pws.web.TerminalWebSocket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
-    private final TerminalWebSocketHandler terminalWebSocketHandler;
-
+    private final TerminalWebSocket terminalWebSocket;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(terminalWebSocketHandler, "/ws/terminal/{sessionId}")
+        registry.addHandler(terminalWebSocket, "/ws/terminal/{sessionId}")
                 .setAllowedOrigins("*"); // replace with allowed origins in prod
     }
 }
